c1dc0375de400924a07e22be9c0a8c6dc9149731
ruuen <max@madebyruuen.com>
2023-05-05 00:03:09 +1000

    Initial project setup
    Need to add local font files and imports, scss variables, and move in media files before starting challenge

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import TipCalculator from "./components/TipCalculator/TipCalculator";
+
+function App() {
+  return (
+    <main className="page-container">
+      <TipCalculator />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/TipCalculator/TipCalculator.jsx
@@ -0,0 +1,3 @@
+export default function TipCalculator() {
+  return <div>TipCalculator</div>;
+}
