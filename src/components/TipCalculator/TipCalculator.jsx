ba4a4275e153197436fd34977707a69f1da3e5e9
ruuen <max@madebyruuen.com>
2023-05-05 17:18:21 +1000

    Moved state up from TipInputs to TipCalculator component

--- src/components/TipCalculator/TipCalculator.jsx
+++ src/components/TipCalculator/TipCalculator.jsx
@@ -1,10 +1,48 @@
+import { useState } from "react";
 import TipInputs from "../TipInputs/TipInputs";
 import TipResults from "../TipResults/TipResults";
 
 export default function TipCalculator() {
+  const [formState, setFormState] = useState([
+    {
+      id: 1,
+      fieldName: "bill",
+      label: "Bill",
+      value: 0,
+    },
+    {
+      id: 2,
+      fieldName: "tipPercent",
+      label: "Select Tip %",
+      value: 5,
+      source: "preset",
+    },
+    {
+      id: 3,
+      fieldName: "numberOfPeople",
+      label: "Number of People",
+      value: 0,
+    },
+  ]);
+
+  function handleChange(e, additionalValues = {}) {
+    const { name, value } = e.target;
+    const predicate = (field) => field.fieldName === name;
+
+    setFormState((prevFormState) => {
+      const newFormState = Array.from(prevFormState);
+      newFormState.splice(newFormState.findIndex(predicate), 1, {
+        ...newFormState.find(predicate),
+        ...additionalValues,
+        value: value,
+      });
+      return newFormState;
+    });
+  }
+
   return (
     <main className="calculator">
-      <TipInputs />
+      <TipInputs formState={formState} handleChange={handleChange} />
       <TipResults />
     </main>
   );
--- src/components/TipInputs/TipInputs.jsx
+++ src/components/TipInputs/TipInputs.jsx
@@ -2,44 +2,7 @@ import { useState } from "react";
 import NumberInput from "../NumberInput/NumberInput";
 import ButtonGroupInput from "../ButtonGroupInput/ButtonGroupInput";
 
-export default function TipInputs() {
-  const [formState, setFormState] = useState([
-    {
-      id: 1,
-      fieldName: "bill",
-      label: "Bill",
-      value: 0,
-    },
-    {
-      id: 2,
-      fieldName: "tipPercent",
-      label: "Select Tip %",
-      value: 5,
-      source: "preset",
-    },
-    {
-      id: 3,
-      fieldName: "numberOfPeople",
-      label: "Number of People",
-      value: 0,
-    },
-  ]);
-
-  function handleChange(e, additionalValues = {}) {
-    const { name, value } = e.target;
-    const predicate = (field) => field.fieldName === name;
-
-    setFormState((prevFormState) => {
-      const newFormState = Array.from(prevFormState);
-      newFormState.splice(newFormState.findIndex(predicate), 1, {
-        ...newFormState.find(predicate),
-        ...additionalValues,
-        value: value,
-      });
-      return newFormState;
-    });
-  }
-
+export default function TipInputs({ formState, handleChange }) {
   const inputElements = formState.map((field) => {
     switch (field.fieldName) {
       case "bill":
